08633c4400aa5d6bb04ebbdfe6158bf885c31305
Joyce-Acia <aciacia06@gmail.com>
2025-06-11 09:42:31 +0700

    im tired

--- server.js
+++ server.js
@@ -2,8 +2,19 @@ var http = require('http');
 // request, response
 var server = http.createServer(function (req, res) {
     res.writeHead(200, {'Content-Type': 'text/html'});
-    res.end("<h1>Hi, Welcome to the Node.js server!</h1>");
-    res.end();
+    switch (req.url) {
+        case '/about':
+            res.write('<h1>This is Home/About Page</h1>');
+            break;
+        case '/profile':
+            res.write('<h1>This is About Page</h1>');
+            break;
+        case '/product':
+            res.write('<h1>This is Product Page</h1>');
+            break;
+        default:
+            res.write('<h1>404 Not Found...Why Is It Called 404? Cause 4 Means Dead!!! plz laugh</h1>');
+    }
 });
 
 server.listen(8000);
